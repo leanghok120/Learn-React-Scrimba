9c00e93bb47151744e663369e68f80f10f33e344
leanghok120 <leanghokoeng5@gmail.com>
2024-05-12 07:28:15 +0700

    Add props to card instead of hardcoding the data

--- AirBnb-clone/src/App.jsx
+++ AirBnb-clone/src/App.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-import Navbar from "./Navbar";
 import Hero from "./Hero";
+import Navbar from "./Navbar";
 import Card from "./Card";
+
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
+
 
 export default function App() {
   return (
     <div className="container">
       <Navbar />
       <Hero />
-      <Card /> 
+      <Card 
+        img="src/assets/katie-zaferes.png"
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      /> 
     </div>
   );
 }
