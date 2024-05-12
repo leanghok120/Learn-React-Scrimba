6eb04bf3f75f941c327486f6d7d413243597be6d
leanghok120 <leanghokoeng5@gmail.com>
2024-05-12 11:17:53 +0700

    Add badge text and clean up card props

--- AirBnb-clone/src/App.jsx
+++ AirBnb-clone/src/App.jsx
@@ -4,29 +4,12 @@ import Navbar from "./Navbar";
 import Card from "./Card";
 import data from "./data";
 
-/*
-Challenge:
-
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
-
 export default function App() {
   const cardElements = data.map((profile) => {
     return (
       <Card
         key={profile.id}
-        img={profile.coverImg}
-        rating={profile.stats.rating}
-        reviewCount={profile.stats.reviewCount}
-        location={profile.location}
-        title={profile.title}
-        price={profile.price}
-        openSpots={profile.openSpots}
+        profile={profile}
       />
     );
   });
--- AirBnb-clone/src/Card.jsx
+++ AirBnb-clone/src/Card.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 
 export default function Card(props) {
+  let badgeText
+  let data = props.profile
+
+  if (data.openSpots === 0) {
+    badgeText = "Sold out"
+  } else if (data.location === "Online") {
+    badgeText = "Online"
+  }
   return (
     <div className="card">
+      {badgeText && <p className="card-badge">{badgeText}</p>}
+
       <div className="img-container">
-        <img src={`public/assets/${props.img}`} alt="card-img" className="card-img" />
+        <img src={`public/assets/${data.coverImg}`} alt="card-img" className="card-img" />
       </div>
       <div className="about-card">
         <div className="stars">
           <img src="public/assets/star.png" alt="star-img" className="star-img" />
           <p className="star-rating">
-            <span>{props.rating}</span> ({props.reviewCount}) • {props.location}
+            <span>{data.stats.rating}</span> ({data.stats.reviewCount}) • {data.location}
           </p>
         </div>
         <div className="desc">
-          <p>{props.title}</p>
+          <p>{data.title}</p>
         </div>
         <div className="cost">
           <p>
-            <span>From ${props.price}</span> / person
+            <span>From ${data.price}</span> / person
           </p>
         </div>
       </div>
