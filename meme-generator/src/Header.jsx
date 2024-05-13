65e3c2b060f19477e35ea8650c01a2fbd30b2af6
leanghok120 <leanghokoeng5@gmail.com>
2024-05-14 06:42:16 +0700

    Style header componenet

--- meme-generator/src/Header.jsx
+++ meme-generator/src/Header.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function Header() {
   return (
     <div className="header">
-      <img src="public/assets/troll-face.png" alt="troll face" />
-      <h1 className="header-title">Meme Generator</h1>
+      <div className="header-left">
+        <img src="public/assets/troll-face.png" alt="troll face" />
+        <h1 className="header-title">Meme Generator</h1>
+      </div>
       <p className="course-title">React Course - Project 3</p>
     </div>
   );
