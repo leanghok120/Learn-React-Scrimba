3ab79e394f5253a8109d93d0a6670790b4dc6ec8
leanghok120 <leanghokoeng5@gmail.com>
2024-05-13 19:51:33 +0700

    Add header component (barebone)

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react"
+import Header from "./Header"
 
 export default function App() {
   return (
-    <h1>Hello, WOrld!</h1>
+    <div className="container">
+      <Header />
+    </div>
   )
 }
---
+++ meme-generator/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img src="public/assets/troll-face.png" alt="troll face" />
+      <h1 className="header-title">Meme Generator</h1>
+      <p className="course-title">React Course - Project 3</p>
+    </div>
+  );
+}
