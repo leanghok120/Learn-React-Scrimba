5122c51cda80e33330e60740ea77aa24eed92afc
leanghok120 <leanghokoeng5@gmail.com>
2024-05-14 06:43:48 +0700

    Change div to header element for semantic html

--- meme-generator/src/Header.jsx
+++ meme-generator/src/Header.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="header">
+    <header className="header">
       <div className="header-left">
         <img src="public/assets/troll-face.png" alt="troll face" />
         <h1 className="header-title">Meme Generator</h1>
       </div>
       <p className="course-title">React Course - Project 3</p>
-    </div>
+    </header>
   );
 }
